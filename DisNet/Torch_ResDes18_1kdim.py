b6ff9c0a7a3f78886b9a3e3f8bb3b273c00583db
Jamie <jrs596@york.ac.uk>
2022-07-08 19:57:53 +0100

    .

--- DisNet/Torch_ResDes18_1kdim.py
+++ DisNet/Torch_ResDes18_1kdim.py
@@ -6,7 +6,6 @@ import torch.optim as optim
 import numpy as np
 import torchvision
 from torchvision import datasets, models, transforms
-import matplotlib.pyplot as plt
 import time
 import os
 import copy
